8c48674928c35b4171dcd71d11bb4849251ac704
agasheadwait <agasheadwait24@gmail.com>
2024-04-05 16:57:52 -0400

    Added basic structure of backend

---
+++ backend_servers/server.cpp
@@ -0,0 +1,22 @@
+/*
+1. Server reads the config file and reads arguments: -v --> verbose, serverconfig file, serverIndex, datafolder location.
+
+2. It listens to TCP connections on the port
+
+3. On a connection, it creates a new thread.
+
+4. Thread echoes back the input.
+
+*/
+
+// Global Varibles
+
+// THread function
+
+// void
+
+// Main Function
+// int main(int argc, char *argv[])
+// {
+
+// }
